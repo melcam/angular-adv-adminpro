2ff70915d91f2665452d44e0cf69c3d7dd6b1f82
Rommel MelCam <yumelcam.93@gmail.com>
2021-03-22 13:33:24 -0500

    Input and output

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class Grafica1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Grafica1Component {
+  public title1: string = 'No title';
+  public labels1: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Delivery Sales',
+  ];
+  public labels2: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public data1 = [[50, 65, 10]];
+  public data2 = [[300, 300, 300]];
 }
